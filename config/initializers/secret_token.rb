6858e19b8408707340d5733def88470d0e4ccf46
Harris Effron <harris.effron@gmail.com>
2013-08-03 10:58:48 -0400

    Initial commit. Add some sample templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterBrewing::Application.config.secret_token = '073fa21cf033e2b49e6e7f28bd86d14f855ea55ed011f4b21bab6eac94e1d72bc84a23bbb0ae0ed8d7a548a0de7e9b7ff176f492dd5c7d36e9349b2861da856d'

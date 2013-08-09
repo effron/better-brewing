7af1c0dd7f36a92d8a2b89d1e54dd2d1a4c9551f
Harris Effron <harris.effron@gmail.com>
2013-08-09 14:04:40 -0400

    Add sliders for all flavor attributes

--- app/assets/javascripts/flavor_sliders.js
+++ app/assets/javascripts/flavor_sliders.js
@@ -1,41 +1,38 @@
-var Sliders = (function(){
+var FlavorSlider = (function(){
 
   var init = function(selectors){
     _.each(selectors, function(selector){
 
       makeSlider(selector)
     })
-  }
+  };
 
   var makeSlider = function(selector){
+    var input = $(selector);
+    var input_name = selector.slice(1)
+    var slider = $("<div id='" + input_name + "-slider'></div>").insertAfter(input).slider({
+      min: 1,
+      max: 6,
+      range: "min",
+      value: input.val(),
+      slide: function(event, ui) {
+        input.val(ui.value - 1);
+      }
+    });
 
+    input.on("change", function() {
+      $(this).val(parseInt($(this).val() || 0));
+      slider.slider( "value", parseInt($(this).val()) + 1 );
+    });
 
-  }
+    input.on("keyup", function() {
+      slider.slider( "value", parseInt($(this).val() || 0) + 1 );
+    });
+
+  };
 
   return {
     init: init
   }
 
 })()
-
-var input = $( "#alcohol" );
-
-var slider = $( "<div id='alcohol-slider'></div>" ).insertAfter( input ).slider({
-  min: 1,
-  max: 6,
-  range: "min",
-  value: input.val(),
-  slide: function( event, ui ) {
-    input.val(ui.value - 1);
-  }
-});
-
-$( "#alcohol").on("change", function() {
-  console.log("fired")
-  $(this).val(parseInt($(this).val() || 0));
-  slider.slider( "value", parseInt($(this).val()) + 1 );
-});
-
-$( "#alcohol").on("keyup", function() {
-  slider.slider( "value", parseInt($(this).val() || 0) + 1 );
-});

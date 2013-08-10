59e5bad4440bf2bd7b064d9c03ba7e2220be6e38
Harris Effron <harris.effron@gmail.com>
2013-08-10 15:28:40 -0400

    Refactor flavor wheel into revealing module pattern

--- app/assets/javascripts/flavor_wheel.js
+++ app/assets/javascripts/flavor_wheel.js
@@ -1,29 +1,40 @@
-var flavorWheel = function(id, names, values, animation){
-
-  var ctx = $(id).get(0).getContext("2d");
-
-  var data = {
-    labels: names,
-    datasets: [
-      {
-        fillColor : "rgba(17,15,46,.5)",
-  			strokeColor : "rgba(23,22,39,1)",
-        data: values
-      }
-    ]
+var FlavorWheel = (function(){
+  
+  var ctx, data, options;
+  
+  var init = function(canvasId, names, values, animation){
+    ctx = $(canvasId).get(0).getContext("2d");
+    
+    data = parseInfo(names, values);
+    
+    options = {
+      scaleOverride: true,
+      scaleSteps: 5,
+      scaleStepWidth: 1,
+      scaleStartValue: 0,
+      scaleShowLabels: true,
+      animation: animation,
+      animationSteps: 45,
+      animationEasing: "easeOutBounce"
+    };
+  
+    new Chart(ctx).Radar(data, options);
   }
-  var options = {
-    scaleOverride: true,
-
-    scaleSteps: 5,
-    scaleStepWidth: 1,
-    scaleStartValue: 0,
-    scaleShowLabels: true,
-    animation: animation,
-    animationSteps: 45,
-    animationEasing: "easeOutBounce"
+  
+  var parseInfo = function(names, values){
+    return {
+      labels: names,
+      datasets: [
+        {
+          fillColor : "rgba(17,15,46,.5)",
+    			strokeColor : "rgba(23,22,39,1)",
+          data: values
+        }
+      ]
+    };
   }
-
-  new Chart(ctx).Radar(data, options);
-
-}
+  
+  return {
+    init: init
+  }  
+})()

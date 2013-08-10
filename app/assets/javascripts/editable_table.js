29b91ea0e6b2e615f43dd5f9c290de096fa3c50c
Harris Effron <harris.effron@gmail.com>
2013-08-10 14:14:56 -0400

    Refactor mash chart drawing, fix editable table bug (switch ids for classes, narrow jquery selector)

--- app/assets/javascripts/editable_table.js
+++ app/assets/javascripts/editable_table.js
@@ -6,17 +6,18 @@ var initializeEditableTable = function(){
 
   $(".editable-table").on("ajax:success", ".update-row", function(event, data){
     var id = $(event.target).data("id");
-    $("#editable-row-" + id).replaceWith(data);
+    $(event.target).parents(".editable-row-" + id).replaceWith(data);
+    // $(".editable-row-" + id).replaceWith(data);
   });
 
   $(".editable-table").on("ajax:success", ".edit-row", function(event, data){
     var id = $(event.target).data("id");
-    $("#editable-row-" + id).replaceWith(data);
+    console.log(id)
+    $(event.target).parents(".editable-row-" + id).replaceWith(data);
   });
 
   $(".editable-table").on("ajax:success", ".destroy-row", function(event, data){
     var id = $(event.target).find(".destroy-button").data("id");
-    console.log(id);
-    $("#editable-row-" + id).remove();
+    $(event.target).parents(".editable-row-" + id).remove();
   });
 }

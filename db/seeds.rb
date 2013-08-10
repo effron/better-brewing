00b9973a712bc96d55618f9e6533fd31c71c7e2d
Harris Effron <harris.effron@gmail.com>
2013-08-10 18:51:46 -0400

    update seeds.rb to just contain important data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = []
-10.times do |n|
-  users << {username: "User_#{n+1}", email: "user_#{n+1}@example.com",
-             password: "password", password_confirmation: "password"}
-end
+# users = []
+# 10.times do |n|
+#   users << {username: "User_#{n+1}", email: "user_#{n+1}@example.com",
+#              password: "password", password_confirmation: "password"}
+# end
+# 
+# User.create!(users)
 
-User.create!(users)
-
-xml = File.read("./public/example.xml")
-xml2 = ""
-open("http://www.brewtoad.com/recipes/firework-cream-ale.xml") do |file|
-  xml2 = file.read
-end
-
-Recipe.create!({name: "test_recipe", user_id: 1, xml: xml })
-Recipe.create!({name: "hopville_recipe", user_id: 2, xml: xml2})
+# xml = File.read("./public/example.xml")
+# xml2 = ""
+# open("http://www.brewtoad.com/recipes/firework-cream-ale.xml") do |file|
+#   xml2 = file.read
+# end
+# 
+# Recipe.create!({name: "test_recipe", user_id: 1, xml: xml })
+# Recipe.create!({name: "hopville_recipe", user_id: 2, xml: xml2})
 
 consumption_vessels = [
   {name: "Shaker Pint"},

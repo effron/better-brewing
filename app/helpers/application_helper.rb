08425d416b0076c896568dd37ae6a50e6bca347c
Harris Effron <harris.effron@gmail.com>
2013-08-11 19:52:39 -0400

    Fix default_url in avatar_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   
   def avatar_url(user)
     default_url = "#{root_url}#{asset_path "default.png"}"
-    default_url = "#{root_url}images/guest.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
   end

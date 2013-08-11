482692d2b25beac157865db3c917e3ab6e652cc6
Harris Effron <harris.effron@gmail.com>
2013-08-11 19:46:17 -0400

    Add gravatar to user index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def avatar_url(user)
+    default_url = "#{root_url}#{asset_path "default.png"}"
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
+  end
+  
 end

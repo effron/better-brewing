725073abdd2c4f841289a52f987ef41a74e368b2
Harris Effron <harris.effron@gmail.com>
2013-08-12 16:28:25 -0400

    Delete and update tasting notes. Deal with multiple tasting notes per brew note

--- app/controllers/tasting_notes_controller.rb
+++ app/controllers/tasting_notes_controller.rb
@@ -6,24 +6,24 @@ class TastingNotesController < ApplicationController
       @tasting_notes = current_user.tasting_notes
     end
   end
-  
+
   def new
     @tasting_note = BrewNote.find(params[:brew_note_id]).tasting_notes.new
-    
+
     unless current_user.brew_notes.include?(@tasting_note.brew_note)
-      raise "NOT YOUR NOTE" 
+      raise "NOT YOUR NOTE"
     end
-    
+
   end
-  
+
   def create
     @tasting_note = BrewNote.find(params[:brew_note_id]).tasting_notes.
                              build(params[:tasting_note])
-    
+
     unless current_user.brew_notes.include?(@tasting_note.brew_note)
-      raise "NOT YOUR NOTE" 
+      raise "NOT YOUR NOTE"
     end
-    
+
     if @tasting_note.save
       redirect_to @tasting_note
     else
@@ -31,40 +31,40 @@ class TastingNotesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @tasting_note = TastingNote.find(params[:id])
   end
-  
+
   def edit
     @tasting_note = TastingNote.find(params[:id])
     unless current_user.brew_notes.include?(@tasting_note.brew_note)
-      raise "NOT YOUR NOTE" 
+      raise "NOT YOUR NOTE"
     end
-  end 
-  
+  end
+
   def update
     @tasting_note = TastingNote.find(params[:id])
-    
+
     unless current_user.brew_notes.include?(@tasting_note.brew_note)
-      raise "NOT YOUR NOTE" 
+      raise "NOT YOUR NOTE"
     end
-    
+
     @tasting_note.update_attributes(params[:tasting_note])
     redirect_to @tasting_note
   end
-  
+
   def destroy
-    @tasting_note = TastingNote.find(params[:id])    
-    
+    @tasting_note = TastingNote.find(params[:id])
+
     unless current_user.brew_notes.include?(@tasting_note.brew_note)
-      raise "NOT YOUR NOTE" 
+      raise "NOT YOUR NOTE"
     end
-    
+
     @tasting_note.destroy
-    
-    flash[:notice] = "Deleted tasting note for #{@tasting_note.brew_note.recipe}"
-    
-    redirect_to tasting_notes_url
+
+    flash[:notice] = "Deleted tasting note for #{@tasting_note.brew_note.recipe.name}"
+
+    redirect_to @tasting_note.brew_note
   end
 end

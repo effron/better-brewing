4983adade1c21fd4056d79be93a58e5d325257f9
Harris Effron <harris.effron@gmail.com>
2013-08-13 17:53:59 -0400

    Add some helper messages for UX, general styling fixes

--- app/assets/javascripts/editable_table.js
+++ app/assets/javascripts/editable_table.js
@@ -8,22 +8,20 @@ var EditableTable = (function(){
     $(".editable-table").on("ajax:success", ".update-row", function(event, data){
       var id = $(event.target).data("id");
       $(event.target).parents(".editable-row-" + id).replaceWith(data);
-      // $(".editable-row-" + id).replaceWith(data);
     });
 
     $(".editable-table").on("ajax:success", ".edit-row", function(event, data){
       var id = $(event.target).data("id");
-      console.log(id)
       $(event.target).parents(".editable-row-" + id).replaceWith(data);
     });
 
     $(".editable-table").on("ajax:success", ".destroy-row", function(event, data){
       var id = $(event.target).find(".destroy-button").data("id");
-      $(event.target).parents(".editable-row-" + id).remove();
+      $(event.target).parents(".editable-row-" + id).fadeOut("fast");
     });
-  }  
+  }
   return {
     init: init
   }
-  
+
 })()

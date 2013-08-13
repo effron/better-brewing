b50cdd63c8c9af724334ae67813775b019df3809
Harris Effron <harris.effron@gmail.com>
2013-08-13 10:57:38 -0400

    Some bug fixes, HTML5 compliance

--- app/concerns/unit_conversion.rb
+++ app/concerns/unit_conversion.rb
@@ -1,28 +1,27 @@
 module UnitConversion
   extend ActiveSupport::Concern
 
-  module InstanceMethods
-    def method_missing(name, *args, &block)
-      if name.to_s =~ /_gallons=$/
-        if !args[0].blank?
-          self.send("#{name.to_s[0..-10]}=", (args[0].to_f * 128).to_i)
-        else
-          self.send("#{name.to_s[0..-10]}=", nil)
-        end
-      elsif name.to_s =~/_gallons$/
-        vol = self.send(name.to_s[0..-9])
-        vol ? vol / 128.0 : nil
+  def method_missing(name, *args, &block)
+    if name.to_s =~ /_gallons=$/
+      if !args[0].blank?
+        self.send("#{name.to_s[0..-10]}=", (args[0].to_f * 128).to_i)
       else
-        super(name, *args, &block)
+        self.send("#{name.to_s[0..-10]}=", nil)
       end
-    end
-
-    def respond_to?(method_sym, include_private = false)
-      if method_sym.to_s =~ /_gallons=?$/
-        true
-      else
-        super(method_sym, include_private)
-      end
+    elsif name.to_s =~/_gallons$/
+      vol = self.send(name.to_s[0..-9])
+      vol ? vol / 128.0 : nil
+    else
+      super(name, *args, &block)
     end
   end
+
+  def respond_to?(method_sym, include_private = false)
+    if method_sym.to_s =~ /_gallons=?$/
+      true
+    else
+      super(method_sym, include_private)
+    end
+  end
+
 end

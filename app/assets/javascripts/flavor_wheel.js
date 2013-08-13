691270a887d888bd1ec0840df55cb0237ae5ce5b
Harris Effron <harris.effron@gmail.com>
2013-08-13 16:23:17 -0400

    Refactor flavor wheels, resizing in brew note compare works

--- app/assets/javascripts/flavor_wheel.js
+++ app/assets/javascripts/flavor_wheel.js
@@ -1,40 +1,71 @@
 var FlavorWheel = (function(){
-  
-  var ctx, data, options;
-  
-  var init = function(canvasId, names, values, animation){
-    ctx = $(canvasId).get(0).getContext("2d");
-    
-    data = parseInfo(names, values);
-    
-    options = {
-      scaleOverride: true,
-      scaleSteps: 5,
-      scaleStepWidth: 1,
-      scaleStartValue: 0,
-      scaleShowLabels: true,
-      animation: animation,
-      animationSteps: 45,
-      animationEasing: "easeOutBounce"
+  var Wheel = function(canvasId, names, values, animation){
+
+    this.canvasId = canvasId;
+    this.names = names;
+    this.values = values;
+    this.animation = animation;
+
+    var ctx, data, options, container, canvas;
+
+    this.init = function(){
+      container = $(canvasId).parent();
+      canvas = $(canvasId)[0];
+      ctx = $(canvasId).get(0).getContext("2d");
+
+      data = parseInfo(names, values);
+
+      bindEvents();
+      resizeCanvas();
+      draw(true);
     };
-  
-    new Chart(ctx).Radar(data, options);
-  }
-  
-  var parseInfo = function(names, values){
-    return {
-      labels: names,
-      datasets: [
-        {
-          fillColor : "rgba(0,57,143,.5)",
-    			strokeColor : "rgba(22,39,65,1)",
-          data: values
-        }
-      ]
+
+    this.off = function(){
+      $(window).off('resize', container)
+    }
+
+    var bindEvents = function(){
+      $(window).on('resize', container, resizeCanvas)
+      console.log(container)
+    }
+
+    var draw = function(animation){
+      options = {
+        scaleOverride: true,
+        scaleSteps: 5,
+        scaleStepWidth: 1,
+        scaleStartValue: 0,
+        scaleShowLabels: true,
+        animation: animation,
+        animationSteps: 45,
+        animationEasing: "easeOutBounce"
+      };
+
+      new Chart(ctx).Radar(data, options);
+    }
+
+    var parseInfo = function(names, values){
+      return {
+        labels: names,
+        datasets: [
+          {
+            fillColor : "rgba(0,57,143,.5)",
+      			strokeColor : "rgba(22,39,65,1)",
+            data: values
+          }
+        ]
+      };
+    }
+
+    var resizeCanvas = function(){
+      var width = container.width();
+      canvas.width = width;
+      canvas.height = width;
+      draw(false);
     };
   }
-  
+
   return {
-    init: init
-  }  
+    Wheel: Wheel
+  }
 })()
